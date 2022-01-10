22ecfc21b62ee90545ff0ac4d83bf5257794bf41
akatief <f.tib29@gmail.com>
2022-01-10 13:46:59 +0100

    chore: remove KernelMethod base class to add it back later

--- qlkit/algorithms/qsvm/qsvc.py
+++ qlkit/algorithms/qsvm/qsvc.py
@@ -1,18 +1,18 @@
 import logging
 from typing import Optional, Union
 import numpy as np
+from qiskit_machine_learning.kernels import QuantumKernel
 from sklearn.base import ClassifierMixin
 from sklearn.exceptions import NotFittedError
 from qiskit.utils import QuantumInstance
 from qiskit.providers import BaseBackend, Backend
 from qiskit.circuit.library import NLocal, ZZFeatureMap
-from ..kernel_method import KernelMethod
 from ..quantum_estimator import QuantumEstimator
 
 logger = logging.getLogger(__name__)
 
 
-class QSVClassifier(ClassifierMixin, QuantumEstimator, KernelMethod):
+class QSVClassifier(ClassifierMixin, QuantumEstimator):
     r"""
     The Quantum Support Vector Machine algorithm for classification.
     Maps datapoints to quantum states using a FeatureMap or similar
@@ -92,6 +92,8 @@ class QSVClassifier(ClassifierMixin, QuantumEstimator, KernelMethod):
         self.alpha = None
         self.bias = None
         self.n_classes = None
+        self.train_kernel_matrix = None
+
 
     def fit(self, X, y):
         """
@@ -144,7 +146,8 @@ class QSVClassifier(ClassifierMixin, QuantumEstimator, KernelMethod):
         for _X, _y in zip(X, y):
             logger.info("%s: %s", _X, _y)
         # Sets the training matrix to None to signal it must be computed again
-        self._reset_train_matrix()
+        self.train_kernel_matrix = None
+
 
     @staticmethod
     def _create_label_class_dicts(labels):
@@ -200,6 +203,39 @@ class QSVClassifier(ClassifierMixin, QuantumEstimator, KernelMethod):
         bias = X[0, :]
         alpha = np.squeeze(X[1:, :])
         return alpha, bias
+
+    def _compute_kernel_matrices(self, X_train, X_test):
+        """
+        Computes the kernel matrices of distances between each training datapoint
+        and between training and test datapoints.
+        Takes advantage of quantum circuits for faster computation.
+
+        Args:
+            X_train: the training data
+            X_test: the unclassified input data
+        Returns:
+            ndarray of train and test kernel matrices
+        """
+        q_kernel = QuantumKernel(feature_map=self._encoding_map,
+                                 quantum_instance=self.quantum_instance)
+
+        if self.train_kernel_matrix is None:
+            n_train = X_train.shape[0]
+
+            # Train and test data stacked together to run backend only once
+            X_total = np.vstack([X_train, X_test])
+
+            total_kernel_matrix = q_kernel.evaluate(x_vec=X_train, y_vec=X_total)
+
+            # Splitting the total matrix into training and test part
+            self.train_kernel_matrix = total_kernel_matrix[:, 0:n_train]
+            # Transposed for ease of use later on
+            test_kernel_matrix = total_kernel_matrix[:, n_train:].T
+        else:
+            # Only the test kernel matrix is needed as the train one has already been computed
+            test_kernel_matrix = q_kernel.evaluate(x_vec=X_test, y_vec=X_train)
+
+        return self.train_kernel_matrix, test_kernel_matrix
 
     def _compute_predictions_multiclass(self, train_kernel_matrix, test_kernel_matrix):
         """
